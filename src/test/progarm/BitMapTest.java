cdc72dfe111f8271139b59d1785f1d4a859b63fa
liyizheng <liyizheng@LIYIZHENG>
2019-07-15 16:43:49 +0800

    fix bug about BitMapTest.java

--- src/test/progarm/BitMapTest.java
+++ src/test/progarm/BitMapTest.java
@@ -3,14 +3,16 @@ package test.progarm;
 public class BitMapTest {
 
     public static void main(String[] args) {
-        int [] arr = new int [] {1, 1, 2, 3, 4, 33, 33, 16};
+        int [] arr = new int [] { 15, 1, 1, 2, 3, 4, 33, 33, 1, 16, 17, 2147483646, 2147483647};
         BitMapTest bitMapTest = new BitMapTest();
+        long currentTime = System.currentTimeMillis(); 
         int [] result = bitMapTest.getExitsNumArr(arr);
         bitMapTest.printNumber(result);
+        System.out.println("used time: " + (System.currentTimeMillis() - currentTime));
     }
     
     public int[] getExitsNumArr(int [] data) {
-        int [] bitArr = new int[Integer.MAX_VALUE >> 4];
+        int [] bitArr = new int[1 << 27];
         for (int i = 0; i < data.length; i++) {
             int temp = data[i];
             int mod = temp % 16;
@@ -32,7 +34,7 @@ public class BitMapTest {
     
     private int getOriginValue(int bucketIndex, int bitIndex) {
         int result = -1;
-        bitIndex = bitIndex > 0 ? (15 - (bitIndex >> 1)) : bitIndex;
+        bitIndex = bitIndex > 0 ? (15 - (bitIndex >> 1)) : 15;
         result = (bucketIndex << 4) + bitIndex;
         return result;
     }
@@ -43,7 +45,13 @@ public class BitMapTest {
                 int filter = 2 << j;
                 int currentValue = (bitArr[i] & filter);
                 if (currentValue != 0) {
-                    System.out.println(getOriginValue(i, j));
+                    System.out.println("appear more than once: " + getOriginValue(i, j));
+                    continue;
+                }
+                
+                int sigleFilter = 1 << j;
+                if ((bitArr[i] & sigleFilter) != 0) {
+                    System.out.println("only appear once: " + getOriginValue(i, j));
                 }
             }
         }
